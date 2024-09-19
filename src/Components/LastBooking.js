44413fdcaeb21ff3a1af9c7fa3b73d0f2dd138af
harshdev2909 <harsh.dev2909@gmail.com>
2024-09-19 20:42:37 +0530

    added

--- src/Components/LastBooking.js
+++ src/Components/LastBooking.js
@@ -7,13 +7,14 @@ const LastBooking = () => {
       <h2 className='heading'>Last Booking</h2>
       <div className='seat-container'>
         <p className='header'>Seats:</p>
-        <ul className='seats'>
-          {seats.map((seats,index)=>{
-            <li className='seat_value' key={index}>
-             (seat) : 0
-            </li>
-          })}
-        </ul>
+        <ul className="seats">
+              {seats.map((seat, index) => (
+                // Display each seat and its count
+                <li className="seat_value" key={index}>
+                  (seat) : 0
+                </li>
+              ))}
+            </ul>
       </div>
       <p className='slots' style={{textAlign:"left"}}>Slot : 00:00 AM<span></span></p>
       <p className='movie'> Movie : <span>Tenet</span></p>
