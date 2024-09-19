2395cf83864110ed362b62bdffaf1b6e6011e667
harshdev2909 <harsh.dev2909@gmail.com>
2024-09-19 20:18:13 +0530

    added

--- src/Components/LastBooking.js
+++ src/Components/LastBooking.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { seats } from '../data'
 
 const LastBooking = () => {
   return (
-    <div>LastBooking</div>
+    <div className='last_booking'>
+      <h2 className='heading'>Last Booking</h2>
+      <div className='seat-container'>
+        <p className='header'>Seats:</p>
+        <ul className='seats'>
+          {seats.map((seats,index)=>{
+            <li className='seat_value' key={index}>
+             (seat) : 0
+            </li>
+          })}
+        </ul>
+      </div>
+      <p className='slots' style={{textAlign:"left"}}>Slot : 00:00 AM<span></span></p>
+      <p className='movie'> Movie : <span>Tenet</span></p>
+    </div>
   )
 }
 
