ca6bfed35b248e245ae1b77b561bc0ccd96c4f00
harshdev2909 <harsh.dev2909@gmail.com>
2024-09-28 09:53:58 +0530

    code added

---
+++ server/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const connectDb = async() =>{
+    try{
+
+       await mongoose.connect('mongodb+srv://harshdev2909:harsh9560@cluster0.gkf5y.mongodb.net/?retryWrites=true&w=majority&appName=xmovies')
+       console.log('connected to Database')
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
+module.exports = connectDb
--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express();
-
 const cors = require('cors')
+const connectDb = require('./db')
+const Tickets = require('./schema')
+app.use(cors())
+app.use(express.json())
+connectDb()
 const port = 5000
 app.use('/',(req,res)=>{
     res.send("hello")
 })
-app.listen(port,(req,res)=>{
+// app.use("api",require('./route'))
+app.listen(port,()=>{
     console.log("Server is running")
 })
