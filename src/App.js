065cfd92190861867b3c597498b3c662cc23e57b
harshdev2909 <harsh.dev2909@gmail.com>
2024-10-22 10:13:20 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import BsState from './context/BsState';
 import Main from './Pages/Main'; 
 
