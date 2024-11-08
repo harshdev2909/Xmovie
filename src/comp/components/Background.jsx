0dbade420f0e02e33fe5c5cf96df9fa7a1d68a13
harshdev2909 <harsh.dev2909@gmail.com>
2024-11-08 12:17:56 +0530

    changes

--- src/comp/components/Background.jsx
+++ src/comp/components/Background.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { WavyBackground } from "./ui/wavy-background";
 import { NavbarDemo } from "./Navbar";
+import { ImagesSliderDemo } from "./Image";
 
 export function WavyBackgroundDemo() {
   return (
@@ -16,6 +17,7 @@ export function WavyBackgroundDemo() {
         className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
         Leverage the power of canvas to create a beautiful hero section
       </p>
+      {/* <ImagesSliderDemo className=" m-10"/> */}
     </WavyBackground>
     </>
   );
