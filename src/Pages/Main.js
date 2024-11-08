940e8d1de31421ad95349ddf868b2f3ea2b464ae
harshdev2909 <harsh.dev2909@gmail.com>
2024-11-08 12:10:08 +0530

    changes

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Routes, Route ,useLocation } from 'react-router-dom'; 
 
 import Home from './Home';
-import { NavbarDemo } from '../comp/components/Navbar';
+
+import { WavyBackgroundDemo } from '../comp/components/Background';
 
 const Main = () => {
   const location = useLocation();
   return (
     <>
-    <NavbarDemo/>
-    <Home/>
+    <WavyBackgroundDemo/>
+    {/* <Home/> */}
     </>
   );
 }
---
+++ src/comp/components/Background.jsx
@@ -0,0 +1,22 @@
+
+import React from "react";
+import { WavyBackground } from "./ui/wavy-background";
+import { NavbarDemo } from "./Navbar";
+
+export function WavyBackgroundDemo() {
+  return (
+  <>
+    <WavyBackground className="relative mx-auto pb-40">
+    <NavbarDemo/>
+      <p
+        className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+        Hero waves are cool
+      </p>
+      <p
+        className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
+        Leverage the power of canvas to create a beautiful hero section
+      </p>
+    </WavyBackground>
+    </>
+  );
+}
